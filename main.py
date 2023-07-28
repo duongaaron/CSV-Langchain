020e4b66341fc94ae562329e3610a246b2282a81
Aaron Duong <duongaaron03@gmail.com>
2023-07-27 23:59:29 -0500

    updated code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from langchain.agents import create_csv_agent
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 import os
 import streamlit as st
-
+import tempfile
 
 def main():
     load_dotenv()
@@ -19,17 +19,23 @@ def main():
     st.header("Ask your CSV 📈")
 
     csv_file = st.file_uploader("Upload a CSV file", type="csv")
+    tmp_path = None
     if csv_file is not None:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as tmpfile:
+            tmpfile.write(csv_file.read())
+            tmp_path = tmpfile.name
 
         agent = create_csv_agent(
-            OpenAI(temperature=0), csv_file, verbose=True)
+            ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0), tmp_path, verbose=True)
 
         user_question = st.text_input("Ask a question about your CSV: ")
 
         if user_question is not None and user_question != "":
             with st.spinner(text="In progress..."):
                 st.write(agent.run(user_question))
-
+                
+    if tmp_path:
+        os.remove(tmp_path)
 
 if __name__ == "__main__":
     main()
